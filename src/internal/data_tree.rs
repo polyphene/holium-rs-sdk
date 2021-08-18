c29a8e955d27d9af3e019d817e53d4149235fe04
tchataigner <tom.chataigner@yahoo.fr>
2021-08-19 01:20:42 +0200

    :sparkles: Revamped data tree implementation to fit needs

--- src/internal/data_tree.rs
+++ src/internal/data_tree.rs
@@ -4,8 +4,11 @@
 use anyhow::Result;
 use serde_cbor::to_vec;
 use serde_cbor::Value as CborValue;
+use crate::KeyNode;
+use std::collections::BTreeMap;
+use crate::internal::key_tree::KeyNode;
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 /// Value held by the leaf of a data tree
 pub(crate) enum Value {
     Null,
@@ -17,20 +20,19 @@ pub(crate) enum Value {
 }
 
 impl Value {
-    pub(crate) fn to_cbor_vec(&self) -> Result<Vec<u8>> {
-        let cbor_value = match self {
+    pub(crate) fn to_cbor(&self) -> CborValue {
+        match self {
             Value::Null => CborValue::Null,
             Value::Bool(v) => CborValue::Bool(*v),
             Value::Integer(v) => CborValue::Integer(*v),
             Value::Float(v) => CborValue::Float(*v),
             Value::Bytes(v) => CborValue::Bytes(v.clone()),
             Value::Text(v) => CborValue::Text(v.clone()),
-        };
-        Ok(to_vec(&cbor_value)?)
+        }
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 /// Recursive structure building simple data trees
 pub struct Node {
     pub(crate) value: Option<Value>,
@@ -60,11 +62,38 @@ impl Node {
             CborValue::__Hidden => unreachable!()
         }
     }
+
+    /// Fuse a key tree and a node tree to generate a Cbor structure based them
+    pub fn assign_keys(&self, key_node: &KeyNode) -> CborValue {
+        match &self.value {
+            Some(value) => value.to_cbor(),
+            None => {
+                if &key_node.children.len() > &(0 as usize) {
+                    let mut map: BTreeMap<CborValue, CborValue> = BTreeMap::new();
+
+                    for (i, child) in self.children.iter().enumerate() {
+                        let key = &key_node.children[i];
+                        map.insert(CborValue::Text(String::from(key.value.unwrap())),child.assign_keys(key));
+                    }
+
+                    CborValue::Map(map)
+                } else {
+                    let mut cbor_values: Vec<CborValue> = Vec::new();
+                    for node in self.children.iter() {
+                        cbor_values.push(node.assign_keys(&KeyNode {value: None, children: &[]}));
+                    }
+                    CborValue::Array(cbor_values)
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use std::collections::BTreeMap;
+    use serde_cbor::value::{to_value};
+    use serde::{Serialize};
 
     use super::*;
 
@@ -139,5 +168,87 @@ mod tests {
             )),
             Node { value: Some(Value::Null), children: vec![] }
         )
+    }
+
+    #[test]
+    fn can_assign_map() {
+        #[derive(Eq, PartialEq, Serialize)]
+        struct Structure {
+            key: NestedStructure
+        }
+
+        #[derive(Eq, PartialEq, Serialize)]
+        struct NestedStructure {
+            key: u8
+        }
+
+        // This code is generated in the wasm module while compiling
+        const STRUCTURE_KEYS: KeyNode = KeyNode {
+            value: None,
+            children: &[
+                KeyNode {
+                    value: Some("key"),
+                    children: NESTED_STRUCTURE_KEYS.children
+                }
+            ]
+        };
+
+        // This code is generated in the wasm module while compiling
+        const NESTED_STRUCTURE_KEYS: KeyNode = KeyNode {
+            value: None,
+            children: &[
+                KeyNode {
+                    value: Some("key"),
+                    children: &[]
+                }
+            ]
+        };
+
+        let structure = Structure {
+            key: NestedStructure {
+                key: 0
+            }
+        };
+
+        let structure_cbor = to_value(structure).unwrap();
+
+        let structure_data = Node::new(structure_cbor.clone());
+
+        let structure_assigned = structure_data.assign_keys(&STRUCTURE_KEYS);
+
+        assert_eq!(structure_cbor, structure_assigned);
+
+    }
+
+    #[test]
+    fn can_assign_array() {
+        #[derive(Eq, PartialEq, Serialize)]
+        struct Structure {
+            key: Vec<u8>
+        }
+
+        // This code is generated in the wasm module while compiling
+        const STRUCTURE_KEYS: KeyNode = KeyNode {
+            value: None,
+            children: &[
+                KeyNode {
+                    value: Some("key"),
+                    children: &[]
+                }
+            ]
+        };
+
+        let structure = Structure {
+            key: vec![0, 1, 2 , 3]
+        };
+
+        let structure_cbor = to_value(structure).unwrap();
+
+        let structure_data = Node::new(structure_cbor.clone());
+
+        let structure_assigned = structure_data.assign_keys(&STRUCTURE_KEYS);
+
+        assert_eq!(structure_cbor, structure_assigned);
+
     }
 }
