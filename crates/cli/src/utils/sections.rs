1e73afad4688fcb488197f639ec5bedef8b09616
tchataigner <tom.chataigner@yahoo.fr>
2021-09-16 18:02:12 +0200

    :white_check_mark: Added tests on utils

--- crates/cli/src/utils/sections.rs
+++ crates/cli/src/utils/sections.rs
@@ -85,3 +85,119 @@ fn as_semver(version_as_bytes: &[u8]) -> Result<Version> {
         .into()),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use walrus::CustomSection;
+
+    // Utils
+    #[derive(Debug, Clone)]
+    pub(super) struct VersionCustomSection(String);
+
+    impl CustomSection for VersionCustomSection {
+        fn name(&self) -> &str {
+            SDK_VERSION_SECTION
+        }
+
+        fn data(&self, _ids_to_indices: &IdsToIndices) -> Cow<'_, [u8]> {
+            Cow::Borrowed(self.0.as_bytes())
+        }
+    }
+
+    // Embed a section in a module. In our case it's a version section.
+    pub fn embed_from_module(
+        mut wasm_module: walrus::Module,
+        version: &semver::Version,
+    ) -> walrus::Module {
+        let custom = VersionCustomSection(version.to_string());
+        wasm_module.customs.add(custom);
+
+        wasm_module
+    }
+
+    // Tests
+
+    #[test]
+    fn cannot_isolate_element_from_multiple_elements_slice() {
+        let vector: Vec<u32> = vec![10, 20];
+
+        let result = try_as_one_section(&vector);
+
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn cannot_isolate_element_from_no_elements_slice() {
+        let vector: Vec<u32> = vec![];
+
+        let result = try_as_one_section(&vector);
+
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn can_isolate_element_from_one_element_slice() {
+        let vector: Vec<u32> = vec![10];
+
+        let element = try_as_one_section(&vector).unwrap();
+
+        assert_eq!(&vector[0], element);
+    }
+
+    #[test]
+    fn cannot_convert_non_utf8_bytes_to_version() {
+        let malformed_slice: Vec<u8> = vec![0, 159, 146, 150];
+
+        let conversion = as_semver(&malformed_slice);
+
+        assert!(conversion.is_err());
+    }
+
+    #[test]
+    fn cannot_convert_malformed_bytes_to_version() {
+        let malformed_str = "I am str !";
+
+        let conversion = as_semver(malformed_str.as_bytes());
+
+        assert!(conversion.is_err());
+    }
+
+    #[test]
+    fn can_convert_bytes_to_version() {
+        use std::str::FromStr;
+
+        let version_str = "1.0.0-alpha";
+        let version = semver::Version::from_str(&version_str).unwrap();
+
+        let conversion = as_semver(version_str.as_bytes()).unwrap();
+
+        assert_eq!(version, conversion);
+    }
+
+    #[test]
+    fn can_get_sections_from_module() {
+        use std::str::FromStr;
+
+        let module = Module::default();
+        let module_w_section =
+            embed_from_module(module, &semver::Version::from_str("1.0.0-alpha").unwrap());
+
+        let sections = extract_custom_sections_by_name(&module_w_section, SDK_VERSION_SECTION);
+
+        assert_eq!(sections.len(), 1);
+    }
+
+    #[test]
+    fn can_get_version_from_module() {
+        use std::str::FromStr;
+
+        let module = Module::default();
+        let version = semver::Version::from_str("1.0.0-alpha").unwrap();
+        let module_w_section = embed_from_module(module, &version);
+
+        let extracted_version = extract_from_module(&module_w_section).unwrap();
+
+        assert_eq!(version, extracted_version);
+    }
+}
